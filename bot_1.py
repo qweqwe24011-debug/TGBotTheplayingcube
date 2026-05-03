5d971cd50b559197d02ee580d7645a8866de0e7a
pownkd <qweqwe24011@gmail.com>
2026-05-03 21:46:24 +0500

    Add files via upload

--- bot_1.py
+++ bot_1.py
@@ -1,17 +1,20 @@
-from os import getenv
 import asyncio
+import os
 from aiogram import Bot, Dispatcher
 from dotenv import load_dotenv
-from aiogram.types import Message
+from pathlib import Path
 from royns import router
-load_dotenv()
-load_dotenv(r"C:\Users\pownkd\Documents\прогромирование\тг\bot_token.env")
-TOKEN = getenv('BOT_TOKEN')
-dp= Dispatcher()
+env_path = Path(__file__).parent / ".env"
+load_dotenv(dotenv_path=env_path)
+TOKEN = os.getenv("BOT_TOKEN")
+print("ENV PATH:", env_path)
+if not TOKEN:
+    raise ValueError("BOT_TOKEN не найден")
+dp = Dispatcher()
 dp.include_router(router)
 async def main():
     bot = Bot(token=TOKEN)
     print("бот запущен")
     await dp.start_polling(bot)
-if __name__ == '__main__':
+if __name__ == "__main__":
     asyncio.run(main())
